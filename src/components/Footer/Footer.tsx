3a29f9538821a73d2514b323af8cc7ffd97f8c2e
Uttam B G <Uttam.BG@dayforce.com>
2024-10-30 15:27:08 +0530

    Formspree key updated

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -10,9 +10,9 @@ import instagramIcon from '../../assets/instagram.svg'
 export function Footer() {
   return (
     <Container className="footer">
-      <a href="https://vinayaksingh.in" className="logo">
-        <span>www.vinayak</span>
-        <span>singh.in</span>
+      <a href="/" className="logo">
+        <span>Uttam </span>
+        <span>B G</span>
       </a>
       <div>
         <p>
@@ -22,35 +22,35 @@ export function Footer() {
       </div>
       <div className="social-media">
         <a
-          href="https://www.linkedin.com/in/codevinayak"
+          href="https://www.linkedin.com/in/uttam-bg"
           target="_blank"
           rel="noreferrer"
         >
           <img src={linkedin} alt="Linkedin" />
         </a>
         <a
-          href="https://github.com/CodeVinayak/"
+          href="https://github.com/Uttambg"
           target="_blank"
           rel="noreferrer"
         >
           <img src={githubIcon} alt="GitHub" />
         </a>
         <a
-          href="https://api.whatsapp.com/send/?phone=%2B919630576848&text=Hello+Vinayak"
+          href="https://api.whatsapp.com/send/?phone=%2B9019048379&text=Hello+Uttam"
           target="_blank"
           rel="noreferrer"
         >
           <img src={whatsapp} alt="Whatsapp" />
         </a>
         <a
-          href="https://t.me/CodeVinayak"
+          href="https://t.me/UttamBg"
           target="_blank"
           rel="noreferrer"
         >
           <img src={telegram} alt="telegram" />
         </a>
         <a
-          href="https://www.instagram.com/vinayaksingh.in"
+          href="https://www.instagram.com/uttam_bg.in"
           target="_blank"
           rel="noreferrer"
         >
--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -5,7 +5,7 @@ import { useEffect, useState } from 'react';
 import validator from 'validator';
 
 export function Form() {
-  const [state, handleSubmit] = useForm('xknkpqry');
+  const [state, handleSubmit] = useForm('xqakpkev');
   const [validEmail, setValidEmail] = useState(false);
   const [message, setMessage] = useState('');
 
